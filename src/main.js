ea1a4b4fc7eddd0da029396139704d52b1e628b8
Pavel <it.pavelkozlov@gmail.com>
2022-02-04 21:31:38 +0300

    init commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+import router from './router'
+// import store from './store'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+
+// createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
+
+
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
